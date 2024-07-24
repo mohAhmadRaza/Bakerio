a6d6f9348614f0d1c3c1d58077126a95faba4a0e
AhmadRaza <158742870+mohAhmadRaza@users.noreply.github.com>
2024-07-24 20:17:04 +0500

    Update Bakerio.py

--- Bakerio.py
+++ Bakerio.py
@@ -1,24 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-# Custom CSS for styling the sidebar buttons
-st.markdown("""
-<style>
-div.stButton > button:first-child{
-    background-color: #000066 !important;
-    color: white !important;
-    width: 100% !important;
-    padding: 10px 0px !important;
-    border: none !important;
-    border-radius: 5px !important;
-    font-size: 16px !important;
-}
-div.stButton > button:hover {
-    background-color: gray !important;
-}
-</style>
-""", unsafe_allow_html=True)
-
+# CSS for initial lookups
 st.markdown("""
    <div style="background-color: #000066; text-align: center; padding: 10px;border-radius : 5px"> 
        <h1 style="color:white"; "font-size:70px";"margin-bottom:-49px">Bakerio</h1>
@@ -45,149 +28,148 @@ st.markdown(f"<div class='initial_message'>{content}</div>", unsafe_allow_html=T
 
 file = st.file_uploader("Upload your customers data file - CSV Format", type=["csv"])
 
+# Creating Class For Abstraction and Encapsulation
 
-def load_data(f=file):
-    d = pd.read_csv(f)
-    print(d)
-    return d
+class CustomerManagement:
+    def __init__(self, file):
+        self.file = file
+        self.data = self.load_data()
 
+    def load_data(self):
+        if self.file:
+            data = pd.read_csv(self.file)
+            return data
+        return pd.DataFrame()
 
-def save_data(data):
-    data.to_csv(r'C:\Users\LENOVO\Desktop\Book1.csv', index=False)
+    def save_data(self, data):
+        if self.file:
+            data.to_csv(file, index=False)
 
+    def add_customers(self):
+        data = self.load_data()
+        name = st.text_input("Enter Customer's Name : ")
+        order_id = st.text_input("Enter Customer's ID : ")
+        order_name = st.text_input("Enter What Customer ordered : ")
+        order_data = st.text_input("Date on which customer ordered : ")
+        city = st.text_input("Enter the customer's city name : ")
 
-def add_customers():
-    data = load_data()
-    name = st.text_input("Enter Customer's Name : ")
-    order_id = st.text_input("Enter Customer's ID : ")
-    order_name = st.text_input("Enter What Customer ordered : ")
-    order_data = st.text_input("Date on which customer ordered : ")
-    city = st.text_input("Enter the customer's city name : ")
-
-    if st.button("Add"):
-        new_data = [name, order_id, order_name, order_data, city]
-        data.loc[len(data.index)] = new_data
-        save_data(data)
-        st.success("Customer added successfully, now select again to perform something, Thank You!")
+        if st.button("Add"):
+            new_data = [name, order_id, order_name, order_data, city]
+            data.loc[len(data.index)] = new_data
+            self.save_data(data)
+            st.success("Customer added successfully, now select again to perform something, Thank You!")
 
 
 # Repeat similar structure for other buttons...
-def categorized_by_city():
-    data = load_data()
-    data['Total Customers'] = data.groupby('City Name')['City Name'].transform('count')
-    data = data[['City Name', 'Total Customers']]
-    data.drop_duplicates(inplace=True)
-    if st.button("Categorize By City"):
-        st.write(data)
-        st.success("Now select again to perform something, Thank You!")
+    def categorized_by_city(self):
+        data = self.load_data()
+        data['Total Customers'] = data.groupby('City Name')['City Name'].transform('count')
+        data = data[['City Name', 'Total Customers']]
+        data.drop_duplicates(inplace=True)
+        if st.button("Categorize By City"):
+           st.write(data)
+           st.success("Now select again to perform something, Thank You!")
 
 
-def frequent_ordered_dish():
-    data = load_data()
-    data['Total Orders'] = data.groupby('Order Name')['Order Name'].transform('count')
-    data = data[['Order Name', 'Total Orders']]
-    data.sort_values(by="Total Orders", inplace=True)
-    data.drop_duplicates(inplace=True)
-    if st.button("Frequently Order"):
-        st.write(data)
-        st.success("Now select again to perform something, Thank You!")
-
-
-def same_city_customers():
-    data = load_data()
-    c = st.text_input("Enter City Name : ")
-    if st.button("Find Customers"):
-        data = data.loc[data['City Name'] == c]
-        st.write(data)
-        st.success("Now select again to perform something, Thank You!")
-
-
-def customers_with_same_orders():
-    c = st.text_input("Enter Order Name : ")
-    data = load_data()
-    if st.button("Find Orders"):
-        data = data.loc[data['Order Name'] == c]
-        st.write(data)
-        st.success("Now select again to perform something, Thank You!")
-
-
-def delete_customers():
-    data = load_data()
-    order_id = st.text_input("Enter OrderID of customer : ")
-    if st.button("Delete Customer"):
-        if order_id in data['Order ID']:
-            ind = data.index[data['Order ID'] == order_id].tolist()
-            data.drop(ind, inplace=True)
-            st.title("Remaining Customers")
+    def frequent_ordered_dish(self):
+        data = self.load_data()
+        data['Total Orders'] = data.groupby('Order Name')['Order Name'].transform('count')
+        data = data[['Order Name', 'Total Orders']]
+        data.sort_values(by="Total Orders", inplace=True)
+        data.drop_duplicates(inplace=True)
+        if st.button("Frequently Order"):
             st.write(data)
-            save_data(data)
             st.success("Now select again to perform something, Thank You!")
 
+    def same_city_customers(self):
+        data = self.load_data()
+        c = st.text_input("Enter City Name : ")
+        if st.button("Find Customers"):
+            data = data.loc[data['City Name'] == c]
+            st.write(data)
+            st.success("Now select again to perform something, Thank You!")
 
-def search_customers():
-    data = load_data()
-    order_id = st.text_input("Enter Order ID of the customer : ")
-    if st.button("Customer Details"):
-        ind = data.loc[data['Order ID'] == order_id]
-        st.write(ind)
+    def customers_with_same_orders(self):
+        c = st.text_input("Enter Order Name : ")
+        data = self.load_data()
+        if st.button("Find Orders"):
+            data = data.loc[data['Order Name'] == c]
+            st.write(data)
+            st.success("Now select again to perform something, Thank You!")
 
+    def delete_customers(self):
+        data = self.load_data()
+        order_id = st.text_input("Enter OrderID of customer : ")
+        if st.button("Delete Customer"):
+            if order_id in data['Order ID']:
+                ind = data.index[data['Order ID'] == order_id].tolist()
+                data.drop(ind, inplace=True)
+                st.title("Remaining Customers")
+                st.write(data)
+                self.save_data(data)
+                st.success("Now select again to perform something, Thank You!")
 
-def update_details():
-    data = load_data()
-    order_id = st.text_input("Enter Order ID : ")
-    if st.button("Update Details"):
-        try:
-            order_id = int(order_id)
-            ind = data.index[data['Order ID'] == order_id].tolist()
-            if not ind:
-                st.write("Order ID Not Found")
-            ind = ind[0]
-            menu = ["Update Name", "Update OrderID", "Update OrderName", "Update OrderDate", "Update CityName"]
-            field = st.selectbox("Select Field For Action ", menu)
-            new_value = st.text_input(f"Enter New {field.split()[-1]} : ")
-            if new_value and st.button(f"Confirm Update {field.split()[-1]}"):
-                if field == "Update Name":
-                    data.loc[ind, 'Name'] = new_value
-                elif field == "Update OrderID":
-                    data.loc[ind, 'Order ID'] = new_value
-                elif field == "Update OrderName":
-                    data.loc[ind, 'Order Name'] = new_value
-                elif field == "Update OrderDate":
-                    data.loc[ind, 'Order Date'] = new_value
-                elif field == "Update CityName":
-                    data.loc[ind, 'City Name'] = new_value
-        except ValueError:
-            st.write("Invalid Order ID. Please enter a numeric Order ID.")
+    def search_customers(self):
+        data = self.load_data()
+        order_id = st.text_input("Enter Order ID of the customer : ")
+        if st.button("Customer Details"):
+            ind = data.loc[data['Order ID'] == order_id]
+            st.write(ind)
 
+    def update_details(self):
+        data = self.load_data()
+        order_id = st.text_input("Enter Order ID : ")
+        if st.button("Update Details"):
+            try:
+                order_id = int(order_id)
+                ind = data.index[data['Order ID'] == order_id].tolist()
+                if not ind:
+                    st.write("Order ID Not Found")
+                ind = ind[0]
+                men = ["Update Name", "Update OrderID", "Update OrderName", "Update OrderDate", "Update CityName"]
+                field = st.selectbox("Select Field For Action ", men)
+                new_value = st.text_input(f"Enter New {field.split()[-1]} : ")
+                if new_value:
+                    if new_value and st.button(f"Confirm Update {field.split()[-1]}"):
+                        if field == "Update Name":
+                            data.loc[ind, 'Name'] = new_value
+                        elif field == "Update OrderID":
+                            data.loc[ind, 'Order ID'] = new_value
+                        elif field == "Update OrderName":
+                            data.loc[ind, 'Order Name'] = new_value
+                        elif field == "Update OrderDate":
+                            data.loc[ind, 'Order Date'] = new_value
+                        elif field == "Update CityName":
+                            data.loc[ind, 'City Name'] = new_value
+                        self.save_data(data)
+                        st.info("Successfully Updated")
+            except ValueError:
+                st.write("Invalid Order ID. Please enter a numeric Order ID.")
 
-def view_customers():
-    data = load_data()
-    if st.button("View All"):
-        st.write(data)
+    def view_customers(self):
+        data = self.load_data()
+        if st.button("View All"):
+            st.write(data)
 
+    def add_new_field(self):
+        data = self.load_data()
+        field = input("Add New Field : ")
+        cases = ['Yes', 'No']
+        char = st.selectbox("You Want To Add Values In This Field For Previous Customers", cases)
+        if char == 'Yes':
+            array = [st.text_input(f'Enter {field} for customer: {name}') for name in data['Name']]
+        else:
+            array = [None]*len(data)
+        data[field] = array
+        self.save_data(data)
 
-def add_new_field():
-    data = load_data()
-    field = input("Add New Field : ")
-    cases = ['Yes', 'No']
-    char = st.selectbox("You Want To Add Values In This Field For Previous Customers", cases)
-    array = []
-    if char == 'Yes':
-        for i in data['Name']:
-            val = st.text_input(r'Enter {field} for customer : {i}')
-            array.append(val)
-    else:
-        array = [None]*len(data)
-    data[field] = array
-    save_data(data)
-
-
-def import_customers_data():
-    data = load_data()
-    second_file = st.file_uploader("Upload Your Second Customer's Data File -CSV Formate.", type=["csv"])
-    others = load_data(second_file)
-    data.append(others, ignore_index=True)
-    save_data(data)
+    def import_customers_data(self):
+        data = self.load_data()
+        second_file = st.file_uploader("Upload Your Second Customer's Data File -CSV Formate.", type=["csv"])
+        others = pd.read_csv(second_file)
+        data = pd.concat([data, others], ignore_index=True)
+        self.save_data(data)
+        st.success("Data imported successfully, now select again to perform something, Thank You!")
 
 
 # Sidebar for navigation
@@ -201,75 +183,50 @@ with st.sidebar:
     ]
     choice = st.selectbox("Select Action To Perform", menu)
 
-
-# Custom CSS for styling
-st.markdown("""
-    <style>
-    /* Sidebar background color */
-    .css-1aumxhk {
-        background-color: #f0f0f5;
-    }
-    /* Button styles */
-    .stButton>button {
-        background-color: #007bff;
-        color: white;
-        border-radius: 5px;
-        width: 100%;
-    }
-    div stButton > button:hover {
-        background-color: #0056b3;
-        color: white;
-    }
-    /* Header styles */
-    .css-10trblm {
-        color: #007bff;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
+# Creating a customer having its customer data file
+customer = CustomerManagement(file)
 
 # Main content based on the choice
 if choice == "Add Customer":
     st.header("Add a New Customer")
-    add_customers()
+    customer.add_customers()
 
 elif choice == "Delete Customer":
     st.header("Delete a Customer")
-    delete_customers()
+    customer.delete_customers()
 
 elif choice == "Find Customer on Order ID":
     st.header("Find Customer by Order ID")
-    search_customers()
+    customer.search_customers()
 
 elif choice == "View All Customers":
     st.header("All Customers")
-    view_customers()
+    customer.view_customers()
 
 elif choice == "Update Details":
     st.header("Update Customer Details")
-    update_details()
+    customer.update_details()
 
 elif choice == "Same City Customers":
     st.header("Customers in the Same City")
-    same_city_customers()
+    customer.same_city_customers()
 
 elif choice == "Most Demanding Dish":
     st.header("Most Demanding Dish")
-    frequent_ordered_dish()
+    customer.frequent_ordered_dish()
 
 elif choice == "Customers With Same Order":
     st.header("Customers with the Same Order")
-    customers_with_same_orders()
+    customer.customers_with_same_orders()
 
 elif choice == "Cities With Number Of Customers":
     st.header("Customers by City")
-    categorized_by_city()
+    customer.categorized_by_city()
 
 elif choice == "Import More Data":
     st.header("Import More Customer Data")
-    import_customers_data()
+    customer.import_customers_data()
 
 elif choice == "Add New Field In File":
     st.header("Add a New Field to the File")
-    add_new_field()
-
+    customer.add_new_field()
